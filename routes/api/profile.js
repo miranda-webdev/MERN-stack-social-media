4d69b0592304db06dbb3382536200aebdb5c0cb4
Miranda <codingwith.miranda@gmail.com>
2019-05-09 17:16:55 -0700

    Added delete profile & user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -117,15 +117,14 @@ router.post('/', [
     }
 });
 
-//@route  Get api/profile
+//@route  GET api/profile
 //@desc   Get all profiles
 //@access Public
 
 router.get('/', async (req, res) => {
     try {
-        const profiles = await Profile.find({}).populate('user', ['name', 'avatar']);
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
         res.json(profiles);
-
     } catch (err) {
         console.error(err.message);
         res.status(500).send(`Server Error: ${err.message}`);
@@ -156,6 +155,33 @@ router.get('/user/:user_id', async (req, res) => {
             });
         }
         // res.status(500).send(`Server Error: ${err.message}`);
+    }
+});
+
+
+//@route  DELETE api/profile
+//@desc   Delete profile, user & posts
+//@access Private
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        //@todo - remove users posts
+
+        //remove profile
+        await Profile.findOneAndRemove({
+            user: req.user.id
+        });
+        //remove user
+        await User.findOneAndRemove({
+            _id: req.user.id
+        });
+
+        res.json({
+            msg: 'User deleted '
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send(`Server Error: ${err.message}`);
     }
 });
 
